a430cd374ef1abcf2d11decc97c44e88a0fc3ef3
Sairan <Sairan1994@gmail.com>
2022-01-15 23:10:23 +0600

    Created a simple contact view template

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	"github.com/gorilla/mux"
 )
-var homeTemplate *template.Template
+var (
+	homeTemplate *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -16,9 +19,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func contacts(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
@@ -38,10 +43,14 @@ func main() {
 	if err!=nil {
 		panic(err)
 	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err!=nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
-	r.HandleFunc("/contacts", contacts)
+	r.HandleFunc("/contacts", contact)
 	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
