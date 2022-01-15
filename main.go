9325ba1d266cc8320c503d7ca5421ad9798ef0e7
Sairan <Sairan1994@gmail.com>
2022-01-15 20:18:57 +0600

    main

--- main.go
+++ main.go
@@ -17,9 +17,22 @@ func contacts(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><p>Here is a list of questions thar our users commonly ask</p>")	
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, 404 error</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contacts", contacts)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
