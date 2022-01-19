b1db589a675bede8e1ba1b2ec8cb8cb2d5ba7cfd
Sairan <Sairan1994@gmail.com>
2022-01-20 01:33:34 +0600

    added View

--- main.go
+++ main.go
@@ -1,56 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/views"
 )
 var (
-	homeTemplate *template.Template
-	contactTemplate *template.Template
+	homeView *views.View
+	contactView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><p>Here is a list of questions thar our users commonly ask</p>")	
-}
 
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Sorry, 404 error</h1>")
-}
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles("views/home.gohtml")
-	if err!=nil {
-		panic(err)
-	}
-	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
-	if err!=nil {
-		panic(err)
-	}
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+	
 	r := mux.NewRouter()
-	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contacts", contact)
-	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
